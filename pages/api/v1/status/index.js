291124c57b996b75473f68baa33ee3643392cf15
carlos-asj <darcilemons@gmail.com>
2026-04-08 14:21:19 -0300

    add database configs and tests working

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,42 @@
+import database from "infra/database.js";
+
+async function status(request, response) {
+    try {
+        const updatedAt = new Date().toISOString();
+
+        const dbVersionRes = await database.query("SHOW server_version;");
+        const dbVersionFormat = dbVersionRes.rows[0].server_version;
+
+        const dbName = process.env.POSTGRES_DB;
+
+        const maxConnectionsRes = await database.query("SHOW max_connections;");
+        const maxConnectionsFormat = maxConnectionsRes.rows[0].max_connections;
+
+        const openedConnections = await database.query({
+            text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+            values: [dbName],
+        });
+        const openedConnectionsRes = openedConnections.rows[0].count;
+
+        response.status(200).json({
+            message: "working great!",
+            updated_at: updatedAt,
+            dependencies: {
+                database: {
+                    version: dbVersionFormat,
+                    max_connections: parseInt(maxConnectionsFormat),
+                    opened_connections: openedConnectionsRes,
+                }
+            }
+        });
+    } catch (error) {
+        console.log("\n Erro dentro do catch do controller:")
+        console.error(error);
+
+        response.status(500).json({
+            message: "Internal server error"
+        });
+    };
+};
+
+export default status;
