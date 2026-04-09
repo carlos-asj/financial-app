3a06c9948bdc43eebbaa4c7b2475db572ea2260a
carlos-asj <darcilemons@gmail.com>
2026-04-09 09:34:50 -0300

    add changes by the scripts for

--- infra/database.js
+++ infra/database.js
@@ -1,36 +1,36 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-    let client;
-    try{
-        client = await getNewClient();
-        const result = await client.query(queryObject);
-        return result;
-    } catch (error) {
-        console.log("\nErro dentro do catch do database.js:");
-        console.error(error);
-        throw error;
-    } finally {
-        await client?.end();
-    }
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log("\nErro dentro do catch do database.js:");
+    console.error(error);
+    throw error;
+  } finally {
+    await client?.end();
+  }
 }
 
 async function getNewClient() {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-    });
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
-    await client.connect();
-    return client;
+  await client.connect();
+  return client;
 }
 
 const database = {
-    query,
-    getNewClient,
+  query,
+  getNewClient,
 };
 
 export default database;
